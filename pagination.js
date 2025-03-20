908a1bf2302269d4e6fac5541d9cabe9a3c40f27
shaay98 <nrichardson567@icloud.com>
2025-03-20 02:40:58 +0000

    implementing JS

---
+++ pagination.js
@@ -0,0 +1,39 @@
+"use strict";
+
+let Page = 1
+async function fetchPosts(page) {
+  try {
+    const URL = `https://jsonplaceholder.typicode.com/posts?_page=${page}&_limit=10`;
+    const res = await fetch(URL);
+    const data = await res.json();
+    renderPosts(data)
+  } catch (error) {
+    console.error(error);
+  }
+}
+fetchPosts(1);
+
+
+function renderPosts(posts) {
+  const div = document.getElementById("Posts");
+  div.innerHTML = "";
+  const ul = document.createElement("ul");
+  posts.forEach((name) => {
+    const li = document.createElement("li");
+    li.innerHTML = name.title;
+    ul.appendChild(li);
+  });
+  div.appendChild(ul);
+}
+document.getElementById("btn-1").addEventListener("click", () => {
+  Page++;
+  fetchPosts(Page);
+});
+document.getElementById("btn-2").addEventListener("click", () => {
+  if (Page > 1) {
+    Page--;
+    fetchPosts(Page);
+  }
+});
+
+fetchPosts()
